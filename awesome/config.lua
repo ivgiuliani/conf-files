3aeeeb335e53e672cb71c7ef51e2b86c0efdd369
Ivan Giuliani <giuliani.v@gmail.com>
2025-06-29 16:59:43 +0100

    Remove unused

--- awesome/config.lua
+++
@@ -1,7 +0,0 @@
-terminal = "rxvt-unicode"
-editor = os.getenv("EDITOR") or "vim"
-editor_cmd = terminal .. " -e " .. editor
-browser = "google-chrome"
-font = "Inconsolata 11"
-net_iface = "wlan0"
-
