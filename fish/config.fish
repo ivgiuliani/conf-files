a07d0a5248001e48293222c16a2e4790b9f52401
Ivan Giuliani <ivan@gocardless.com>
2025-07-22 08:53:21 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,47 +1,8 @@
-# Remove welcome message
-set fish_greeting
+set -g fish_greeting
+set -g theme_title_use_abbreviated_path no
 
-# add custom scripts to $PATH
-set PATH $HOME/bin $PATH
-
-# rustup
-set PATH $HOME/.cargo/bin $PATH
-
-# As instructed by brew
-set BYOBU_PREFIX /usr/local
-
-set PYTHONPATH $PYTHONPATH /usr/local/lib/python2.7/site-packages
-
-export NVM_DIR="$HOME/.nvm"
-# /usr/local/opt/nvm/nvm.sh"
-
-fish_add_path "/usr/local/bin"
-
-. ~/.config/fish/autoenv.fish
-if test -e $AUTOENVFISH_FILE
-  . $AUTOENVFISH_FILE
-end
-
-# rb-env
-fish_add_path $HOME/.rbenv/bin
-fish_add_path $HOME/.rbenv/shims
-rbenv rehash 2> /dev/null
-
-# py-env
-fish_add_path $HOME/.pyenv/bin
-fish_add_path $HOME/.pyenv/shims
-pyenv rehash 2> /dev/null
-pyenv init - | source
-
-# golang
-set PATH /usr/local/go/bin $PATH
-set GOPATH /Users/ivan/.go
-export GOPATH=/Users/ivan/.go
-set PATH $GOPATH/bin $PATH
-
-# Fixes python issues with OSX
-# https://stackoverflow.com/a/52230415
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY="YES"
+# Global settings
+# --------------------------------
 
 # Termcap colors
 set -x LESS_TERMCAP_mb (printf "\e[01;31m")
@@ -52,7 +13,49 @@ set -x LESS_TERMCAP_so (printf "\e[01;44;33m")
 set -x LESS_TERMCAP_ue (printf "\e[0m")
 set -x LESS_TERMCAP_us (printf "\e[01;32m")
 
-set -g theme_title_use_abbreviated_path no
+set -x EDITOR /opt/homebrew/bin/nvim
+
+# Paths
+# --------------------------------
+fish_add_path "/usr/local/bin"
+fish_add_path $HOME/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+fish_add_path /opt/homebrew/opt/openjdk/bin # java
+fish_add_path /opt/homebrew/share/google-cloud-sdk/bin # gcloud sdk
+fish_add_path $HOME/.rd/bin # rancher
+fish_add_path $HOME/.rbenv/bin # rbenv
+fish_add_path $HOME/.rbenv/shims # rbenv
+fish_add_path $HOME/.pyenv/bin
+fish_add_path $HOME/.pyenv/shims
+fish_add_path $HOME/.codeium/windsurf/bin # windsurf
+
+
+# As instructed by brew
+set BYOBU_PREFIX /usr/local
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+
+export NVM_DIR="$HOME/.nvm"
+
+rbenv rehash 2> /dev/null
+pyenv rehash 2> /dev/null
+pyenv init - | source
+
+# golang
+set PATH /usr/local/go/bin $PATH
+set GOPATH /Users/ivan/.go
+export GOPATH=/Users/ivan/.go
+set PATH $GOPATH/bin $PATH
+
+. ~/.config/fish/autoenv.fish
+if test -e $AUTOENVFISH_FILE
+  . $AUTOENVFISH_FILE
+end
+
+# Fixes python issues with OSX
+# https://stackoverflow.com/a/52230415
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY="YES"
 
 # Tide prompt
 set --global tide_left_prompt_items pwd git character
@@ -68,3 +71,7 @@ if status is-interactive
   zellij_utils
   zellij_update_tabname
 end
+# Disable command buffer edit via alt-e (still available via alt-v)
+
+bind --preset -e alt-e
+
